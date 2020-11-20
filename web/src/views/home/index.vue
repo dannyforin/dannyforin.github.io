c9bfa57f89e0f773b7731c88dca57400da47ed2c
dannyforin <3190428662@qq.com>
2020-11-20 10:24:43 +0800

    6

--- web/src/views/home/index.vue
+++ web/src/views/home/index.vue
@@ -5,11 +5,30 @@
     <button>hello world</button>
     <button>hello world</button>
     <button>hello world</button>
+    <br />
+    <img :src="url" alt="" width="100" height="100" />
+    {{ url }}
+    {{ id }}
   </div>
 </template>
 
 <script>
-export default {}
+import axios from 'axios'
+export default {
+  data() {
+    return {
+      url: '',
+      id: ''
+    }
+  },
+  created() {
+    axios.get('https://unsplash.it/1600/900?random').then(x => {
+      console.log(x)
+      this.url = x.config.url
+      this.id = x.headers
+    })
+  }
+}
 </script>
 
 <style lang="scss" scoped>
